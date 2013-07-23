026fb70f30852eff0b7e4e2bcc1523f26f358e91
Henning Gross <igaffai@gmail.com>
2013-07-23 09:37:51 +0200

    cleanup

--- Messsystem/VeCom/VeCom/SerialCommunication.h
+++
@@ -1,5 +0,0 @@
-void uart_init();
-void uart_putc(unsigned char c);
-void uart_puts (char *s);
-void sendPacket(int rpm_diff, int v_diff, int egt);
-int addIntToString(char* existingString, int offset, int addMe);
